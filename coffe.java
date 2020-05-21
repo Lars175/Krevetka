be4579118a47e135a0ccd23c59e02eef9b03eba2
Lars175 <63231704+Lars175@users.noreply.github.com>
2020-05-21 17:38:37 +0300

    Create coffe.java

---
+++ coffe.java
@@ -0,0 +1,53 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Кофе-машина");
+		
+		int moneyAmount = 35;
+		
+		int cappucinoPrice = 150;
+		int espressoPrice = 80;
+		int waterPrice = 20;
+		int milkPrice = 40;
+		int kakaoPrice = 60;
+		
+		boolean canBuyAnything = false;
+		
+		if(moneyAmount >= cappucinoPrice) {
+			System.out.println("Вы можете купить капучино");
+			canBuyAnything = true;
+		}
+		
+		if(moneyAmount >= espressoPrice) {
+			System.out.println("Вы можете купить эспрессо");
+			canBuyAnything = true;
+		}
+		
+		if(moneyAmount >= waterPrice) {
+			System.out.println("Вы можете купить воду");
+			canBuyAnything = true;
+		}
+		
+		if(moneyAmount >= milkPrice) {
+			System.out.println("Вы можете купить молоко");
+			canBuyAnything = true;
+		}
+		
+		if(moneyAmount >= kakaoPrice) {
+			System.out.println("Вы можете купить какаво");
+			canBuyAnything = true;
+		}
+		
+		if(canBuyAnything == false) {
+			System.out.println("Недостаточно средств :( Изучайте Java и зарабатывайте много! :)");
+		}
+	}
+}
